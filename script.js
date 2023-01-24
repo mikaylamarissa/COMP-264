e3884f1afc7e2e94b26c6f1767d20e4853a71900
Mikayla Lafond <mikayla.lafond@gmail.com>
2023-01-24 17:46:09 -0600

    made changes to script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 window.onload = function() {
     alert("Welcome to my website, powered with Git!");
   }
+
+  const cars = [];
+cars[0]= "Saab";
+cars[1]= "Volvo";
+cars[2]= "BMW";
+
+console.log(cars);
